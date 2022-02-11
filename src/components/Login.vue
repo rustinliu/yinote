7b9d182eaf71c784dcfa93e0f25d719e73d7e399
rustin <1543153730@qq.com>
2022-02-11 23:06:27 +0800

    添加动效

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,14 +6,16 @@
                     <div class="main"></div>
                     <div class="form">
                         <h3 @click="showRegister">创建账户</h3>
-                        <div v-show="isShowRegister" class="register">
+
+                        <div :class="{ show: isShowRegister }" class="register">
                             <input type="text" v-model="register.username" placeholder="用户名" />
                             <input type="password" v-model="register.password" placeholder="密码" />
                             <p :class="{ error: register.isError }">{{ register.notice }}</p>
                             <div class="button" @click="onRegister">创建账号</div>
                         </div>
                         <h3 @click="showLogin">登录</h3>
-                        <div v-show="isShowLogin" class="login">
+
+                        <div :class="{ show: isShowLogin }" class="login">
                             <input type="text" v-model="login.username" placeholder="输入用户名" />
                             <input type="password" v-model="login.password" placeholder="密码" />
                             <p :class="{ error: login.isError }">{{ login.notice }}</p>
@@ -123,14 +125,14 @@ export default {
     .form {
         width: 270px;
         border-left: 1px solid #ccc;
-
+        overflow: hidden;
         h3 {
             padding: 10px 20px;
             font-weight: normal;
             font-size: 16px;
             border-top: 1px solid #eee;
             cursor: pointer;
-
+            margin-top: -1px;
             &:nth-of-type(2) {
                 border-bottom: 1px solid #eee;
             }
@@ -150,9 +152,15 @@ export default {
 
         .login,
         .register {
-            padding: 10px 20px;
+            padding: 0px 20px;
             border-top: 1px solid #eee;
+            height: 0;
+            overflow: hidden;
+            transition: height 0.4s;
 
+            &.show {
+                height: 193px;
+            }
             input {
                 display: block;
                 width: 100%;
