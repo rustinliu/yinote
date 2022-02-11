1a1e4e61c81d74fad421251dbb096e0c4f8cb8ee
rustin <1543153730@qq.com>
2022-02-11 21:09:07 +0800

    引入SVG图标，完成Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <button @click="back">back</button>
-    <button @click="next">next</button>
-    <router-view />
-  </div>
+    <div id="app">
+        <Sidebar></Sidebar>
+        <router-view />
+    </div>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar.vue';
 export default {
-  name: "App",
-  methods: {
-    back() {
-      this.$router.go(-1);
+    name: 'App',
+    components: {
+        Sidebar,
     },
-    next() {
-      this.$router.go(1);
-    }
-  }
 };
 </script>
 
 <style>
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html,
+body,
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+}
+body {
+    font-family: -apple-system, 'Noto Sans', 'Helvetica Neue', Helvetica, 'Nimbus Sans L', Arial, 'Liberation Sans', 'PingFang SC', 'Hiragino Sans GB', 'Noto Sans CJK SC', 'Source Han Sans SC', 'Source Han Sans CN', 'Microsoft YaHei', 'Wenquanyi Micro Hei', 'WenQuanYi Zen Hei', 'ST Heiti', SimHei, 'WenQuanYi Zen Hei Sharp', sans-serif;
+    font-size: 14px;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #eee;
+}
+a {
+    text-decoration: none;
+    color: #444;
+}
+ul,
+li {
+    list-style: none;
+}
+.btn {
+    color: #666;
+    font-size: 12px;
+    padding: 2px 4px;
+    background-color: #fff;
+    box-shadow: 0px 0px 2px 0px #ccc;
+    border: none;
+    cursor: pointer;
+    display: inline-block;
+}
+#app {
+    display: flex;
+    align-items: stretch;
 }
 </style>
